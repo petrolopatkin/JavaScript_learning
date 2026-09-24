73dcac2c0750fb54c08e23c97301b3cdbf0634a4
petrolopatkin <piter.lopatkin@gmail.com>
2026-09-24 20:31:38 +0200

    Day 1 of my JavaScript journey: const, let, ture/false variables and little practice

---
+++ day_01.js
@@ -0,0 +1,39 @@
+// first code on JavaScript using const, let, true/false variables
+const n = 'Peter'
+let age = 18
+
+console.log(n, age)
+
+const isStudent = true
+const hasJob = false
+
+console.log(isStudent)
+console.log(hasJob)
+
+// Types of data
+console.log(typeof n)
+console.log(typeof age)
+console.log(typeof isStudent)
+
+// Math 
+const a = 6
+const b = 7
+
+console.log(a + b)
+console.log(b - a)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+
+// First pracrice
+// task 1
+let price = 25
+let quantity = 3
+
+console.log(price * quantity)
+
+// task 2
+const nn = 'Peter'
+let city = 'Presov'
+
+console.log(nn + ' ' + 'lives in' + ' ' + city)
